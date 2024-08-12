efccc367153e1a431b8ee6c13feff8b9bfe5c854
Jakob Rühlow <j.ruehlow@campus.tu-berlin.de>
2024-08-12 15:44:13 +0200

    optimize lookuptable generation - reduced time by ~50s

--- matlab/generateIrsLookupTable.m
+++ matlab/generateIrsLookupTable.m
@@ -4,86 +4,81 @@ function ris_table = generateIrsLookupTable(optimal_in_angle, optimal_out_angle,
 %   Nr, Nc - number of rows/columns of the RIS
 %   fc - carrier frequency
 
-    % Set up parameters
-    c = physconst('lightspeed');
-    lambda = c/fc;
-    fs = 10e6; % Sample rate
+% Set up parameters
+c = physconst('lightspeed');
+lambda = c/fc;
 
-    % Setup 
-    dr = 0.5*lambda; % Row spacing
-    dc = 0.5*lambda; % Column spacing
-    % x = 2*randi(2,[100 1])-3;
-    % tx = phased.Transmitter('PeakPower',50e-3,'Gain',0);
-    % xt = tx(x);
+% Setup
+dr = 0.5*lambda; % Row spacing
+dc = 0.5*lambda; % Column spacing
 
-    % Construct RIS
-    ris = helperRISSurface('Size',[Nr Nc],'ElementSpacing',[dr dc],...
-        'ReflectorElement',phased.IsotropicAntennaElement,'OperatingFrequency',fc);
+% Construct RIS
+ris = helperRISSurface('Size',[Nr Nc],'ElementSpacing',[dr dc],...
+    'ReflectorElement',phased.IsotropicAntennaElement,'OperatingFrequency',fc);
 
-    % Create channel models
-    chanAPToRIS = phased.FreeSpace('SampleRate',fs,'PropagationSpeed',c,...
-        'MaximumDistanceSource','Property','MaximumDistance',500, 'OperatingFrequency', fc);
+% Create channel
+chanAPToRIS = phased.FreeSpace('SampleRate',10e6,'PropagationSpeed',c,...
+    'MaximumDistanceSource','Property','MaximumDistance',500, 'OperatingFrequency', fc);
 
-    % Define angle range for lookup table
-    % angles = -89:1:89;
-    angles = 1:1:179;
+% Define angle range for lookup table
+angles = 1:1:179;
 
-    % Preallocate results array
-    num_angles = length(angles);
-    results = zeros(num_angles^2, 4);
+% RIS position
+d2_ris = 10;
+pos_ris = zeros(3,1);
+v = zeros(3,1);
 
-    % RIS position
-    d2_ris = 10;
-    pos_ris = [0; 0; 0];
-    v = [0; 0; 0]; % Static scenario
+stv = getSteeringVector(ris);
 
-    % Optimize RIS for the optimal constellation point
-    stv = getSteeringVector(ris);
-    g = stv(fc, optimal_in_angle);
-    hr = stv(fc, optimal_out_angle);
-    rcoeff_ris_optimal = exp(1i*(-angle(hr)-angle(g)));
+stv = getSteeringVector(ris);
+g = stv(fc, optimal_in_angle);
+hr = stv(fc, optimal_out_angle);
+rcoeff_ris = exp(1i*(-angle(hr)-angle(g)));
 
-    pos_ap = [d2_ris*cosd(optimal_in_angle); d2_ris*sind(optimal_in_angle); 0];
-    x_ris_in = chanAPToRIS(ones(1e3, 1), pos_ap, pos_ris, v, v); % e6
-    % Generate lookup table
-    idx = 1;
-    for in_angle = angles
-        for out_angle = angles
-            % Set positions based on angles
-            pos_ap = [d2_ris*cosd(in_angle); d2_ris*sind(in_angle); 0];
+% Generate lookup table
+[in_angles, out_angles] = meshgrid(angles, angles);
+in_angles = in_angles(:);
+out_angles = out_angles(:);
 
-            % Apply RIS phase control (optimized for optimal constellation) and simulate paths
-            x_ris_out = ris(x_ris_in, in_angle, out_angle, rcoeff_ris_optimal);
+% Position calculation
+pos_ap = [d2_ris*cosd(in_angles)'; d2_ris*sind(in_angles)'; zeros(1, numel(in_angles))];
 
-            % Calculate gain
-            in_power = pow2db(bandpower(x_ris_in));
-            out_power = pow2db(bandpower(x_ris_out));
-            gain_db = out_power-in_power;
-        
-            % Calculate phase shift
-            phase_in = angle(x_ris_in);
-            phase_out = angle(x_ris_out);
-            phase_shift = wrapToPi(phase_out - phase_in);
+% Preallocate results array
+num_angles = numel(in_angles);
+results = zeros(num_angles, 4);
 
-            % Calculate mean value
-            phase_shift = mean(phase_shift);
+% input signal
+x_ris_in = chanAPToRIS(ones(1e3, 1), pos_ap(:,1), pos_ris, v, v);
 
-            % Store results
-            results(idx, :) = [in_angle, out_angle, gain_db, phase_shift];
-            idx = idx + 1;
-            if mod(idx, 2000) == 0
-                fprintf('Processed %d out of %d rows (%.2f%% complete)\n', idx, 32041, (idx/32041)*100);
-            end
-        end
-    end
+% Use parfor for parallel processing
+parfor i = 1:num_angles
+    % Apply RIS phase control and simulate paths
+    x_ris_out = ris(x_ris_in, in_angles(i), out_angles(i), rcoeff_ris);
 
-    % Convert to table
-    ris_table = array2table(results, 'VariableNames', {'in_angle', 'out_angle', 'gain_dB', 'phase_shift'});
+    % Calculate gain
+    in_power = pow2db(bandpower(x_ris_in));
+    out_power = pow2db(bandpower(x_ris_out));
+    gain_db = out_power - in_power;
 
-    filename = sprintf('IRS_%d_IN%d_OUT%d_FREQ%.2fGHz.csv', Nr*Nc, optimal_in_angle, optimal_out_angle, fc/1e9);
-    
-    writetable(ris_table, filename, 'Delimiter', ',', 'WriteRowNames', true);
-    
-    % Display a message to confirm the export
-    disp(['Table has been exported to ', filename]);
+    % Calculate phase shift
+    phase_in = angle(x_ris_in);
+    phase_out = angle(x_ris_out);
+    phase_shift = wrapToPi(phase_out - phase_in);
+
+    % Calculate mean value
+    phase_shift = mean(phase_shift);
+
+    % Store results
+    results(i, :) = [in_angles(i), out_angles(i), gain_db, phase_shift];
+end
+
+% Convert to table
+ris_table = array2table(results, 'VariableNames', {'in_angle', 'out_angle', 'gain_dB', 'phase_shift'});
+
+% Export table
+filename = sprintf('IRS_%d_IN%d_OUT%d_FREQ%.2fGHz_destructive.csv', Nr*Nc, optimal_in_angle, optimal_out_angle, fc/1e9);
+writetable(ris_table, filename, 'Delimiter', ',', 'WriteRowNames', true);
+
+% Display a message to confirm the export
+disp(['Table has been exported to ', filename]);
 end
--- matlab/generateIrsLookupTableConstructive.m
+++ matlab/generateIrsLookupTableConstructive.m
@@ -5,91 +5,86 @@ function ris_table = generateIrsLookupTableConstructive(optimal_in_angle, optima
 %   fc - carrier frequency
 %   r_ue_ap, r_ap_ris, r_ue_ris - distances for the respective paths
 
-    % Set up parameters
-    c = physconst('lightspeed');
-    lambda = c/fc;
-    fs = 10e6; % Sample rate
+% Set up parameters
+c = physconst('lightspeed');
+lambda = c/fc;
 
-    % Setup
-    dr = 0.5*lambda; % Row spacing
-    dc = 0.5*lambda; % Column spacing
+% Setup
+dr = 0.5*lambda; % Row spacing
+dc = 0.5*lambda; % Column spacing
 
-    % Construct RIS
-    ris = helperRISSurface('Size',[Nr Nc],'ElementSpacing',[dr dc],...
-        'ReflectorElement',phased.IsotropicAntennaElement,'OperatingFrequency',fc);
+% Construct RIS
+ris = helperRISSurface('Size',[Nr Nc],'ElementSpacing',[dr dc],...
+    'ReflectorElement',phased.IsotropicAntennaElement,'OperatingFrequency',fc);
 
-    % Create channel models
-    chanAPToRIS = phased.FreeSpace('SampleRate',fs,'PropagationSpeed',c,...
-        'MaximumDistanceSource','Property','MaximumDistance',500, 'OperatingFrequency', fc);
+% Create channel
+chanAPToRIS = phased.FreeSpace('SampleRate',10e6,'PropagationSpeed',c,...
+    'MaximumDistanceSource','Property','MaximumDistance',500, 'OperatingFrequency', fc);
 
-    % Define angle range for lookup table
-    angles = 1:1:179;
+% Define angle range for lookup table
+angles = 1:1:179;
 
-    % Preallocate results array
-    num_angles = length(angles);
-    results = zeros(num_angles^2, 4);
+% RIS position
+d2_ris = 10;
+pos_ris = zeros(3,1);
+v = zeros(3,1);
 
-    % RIS position
-    d2_ris = 10;
-    pos_ris = [0; 0; 0];
-    v = [0; 0; 0]; % Static scenario
+stv = getSteeringVector(ris);
 
-    stv = getSteeringVector(ris);
-    % Calculate steering vectors for input and output angles
-    g = stv(fc, optimal_in_angle);
-    hr = stv(fc, optimal_out_angle);
-    
-    % Calculate the direct path phase
-    direct_path_phase = 2 * pi * r_ap_ue / lambda;
-    % Calculate the reflected path phase
-    reflected_path_phase = 2 * pi * (r_ap_ris + r_ue_ris) / lambda;
-    % Calculate the required phase shift for constructive interference
-    required_phase_shift = (2 * pi) - (reflected_path_phase - direct_path_phase);
-    % Calculate the optimal reflection coefficient
-    rcoeff_ris_optimal = exp(1i * (required_phase_shift - angle(hr) - angle(g)));
-    
-    pos_ap = [d2_ris*cosd(optimal_in_angle); d2_ris*sind(optimal_in_angle); 0];
-    x_ris_in = chanAPToRIS(ones(1e3, 1), pos_ap, pos_ris, v, v); % e6
-    
-    % Generate lookup table
-    idx = 1;
-    for in_angle = angles
-        for out_angle = angles
-            % Set positions based on angles
-            pos_ap = [d2_ris*cosd(in_angle); d2_ris*sind(in_angle); 0];
+% Calculate steering vectors for input and output angles
+g = stv(fc, optimal_in_angle);
+hr = stv(fc, optimal_out_angle);
 
-            % Apply RIS phase control (optimized for optimal constellation) and simulate paths
-            x_ris_out = ris(x_ris_in, in_angle, out_angle, rcoeff_ris_optimal);
+% Calculate the optimal reflection coefficient
+direct_path_phase = 2 * pi * r_ap_ue / lambda;
+reflected_path_phase = 2 * pi * (r_ap_ris + r_ue_ris) / lambda;
+required_phase_shift = (2 * pi) - (reflected_path_phase - direct_path_phase);
+rcoeff_ris = exp(1i * (required_phase_shift - angle(hr) - angle(g)));
 
-            % Calculate gain
-            in_power = pow2db(bandpower(x_ris_in));
-            out_power = pow2db(bandpower(x_ris_out));
-            gain_db = out_power-in_power;
+% Generate lookup table
+[in_angles, out_angles] = meshgrid(angles, angles);
+in_angles = in_angles(:);
+out_angles = out_angles(:);
 
-            % Calculate phase shift
-            phase_in = angle(x_ris_in);
-            phase_out = angle(x_ris_out);
-            phase_shift = wrapToPi(phase_out - phase_in);
+% Position calculation
+pos_ap = [d2_ris*cosd(in_angles)'; d2_ris*sind(in_angles)'; zeros(1, numel(in_angles))];
 
-            % Calculate mean value
-            phase_shift = mean(phase_shift);
+% Preallocate results array
+num_angles = numel(in_angles);
+results = zeros(num_angles, 4);
 
-            % Store results
-            results(idx, :) = [in_angle, out_angle, gain_db, phase_shift];
-            idx = idx + 1;
-            if mod(idx, 2000) == 0
-                fprintf('Processed %d out of %d rows (%.2f%% complete)\n', idx, 32041, (idx/32041)*100);
-            end
-        end
-    end
+% input signal
+x_ris_in = chanAPToRIS(ones(1e3, 1), pos_ap(:,1), pos_ris, v, v);
 
-    % Convert to table
-    ris_table = array2table(results, 'VariableNames', {'in_angle', 'out_angle', 'gain_dB', 'phase_shift'});
+% Use parfor for parallel processing
+parfor i = 1:num_angles
+    % Apply RIS phase control and simulate paths
+    x_ris_out = ris(x_ris_in, in_angles(i), out_angles(i), rcoeff_ris);
 
-    filename = sprintf('IRS_%d_IN%d_OUT%d_FREQ%.2fGHz_constructive.csv', Nr*Nc, optimal_in_angle, optimal_out_angle, fc/1e9);
-    
-    writetable(ris_table, filename, 'Delimiter', ',', 'WriteRowNames', true);
-    
-    % Display a message to confirm the export
-    disp(['Table has been exported to ', filename]);
+    % Calculate gain
+    in_power = pow2db(bandpower(x_ris_in));
+    out_power = pow2db(bandpower(x_ris_out));
+    gain_db = out_power - in_power;
+
+    % Calculate phase shift
+    phase_in = angle(x_ris_in);
+    phase_out = angle(x_ris_out);
+    phase_shift = wrapToPi(phase_out - phase_in);
+
+    % Calculate mean value
+    phase_shift = mean(phase_shift);
+
+    % Store results
+    results(i, :) = [in_angles(i), out_angles(i), gain_db, phase_shift];
+end
+
+% Convert to table
+ris_table = array2table(results, 'VariableNames', {'in_angle', 'out_angle', 'gain_dB', 'phase_shift'});
+
+% Export table
+filename = sprintf('IRS_%d_IN%d_OUT%d_FREQ%.2fGHz_constructive.csv', Nr*Nc, optimal_in_angle, optimal_out_angle, fc/1e9);
+writetable(ris_table, filename, 'Delimiter', ',', 'WriteRowNames', true);
+
+% Display a message to confirm the export
+disp(['Table has been exported to ', filename]);
 end
